665d705002c977e13fd7f4c8f35a6d272d0030d1
Karim <karimblk07@gmail.com>
2021-02-10 19:24:56 +0100

    La fonction pour charger les images a été mise a jour et est maintenant fonctionnel. Implementation de la fonction pour dessiner le canva grace a un tableau recu en parametre. Readme, contient les details sur le tableau d'images.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Labyrinthe
 A nous la réussite. Visca el barcaaa !!
+
+i[0] == A(S)
+i[1] == B(F)
+i[2] == B(S)
+i[3] == C(F)
+i[4] == C(S)
+i[5] == D(F) *
+i[6] == E(F)
+i[7] == E(S)
+i[8] == F(F)
+i[9] == F(S)
+i[10] == G(S)
+
+i[11] == H(S)
+i[12] == I(F)
+i[13] == I(S)
+i[14] == J(F) *
+i[15] == K(F)
+i[16] == K(S)
+i[17] == L(S)
+
+i[18] == M(F)
+i[19] == M(S)
+i[20] == N(F)
+i[21] == O(F)
+i[22] == O(S)
+
+i[23] == P(S)
+i[24] == Q(S)
--- script.js
+++ script.js
@@ -14,13 +14,54 @@ var tab = new Array(25);
 tab=initArray(tab);
 var fontImage;
 
+function indexOfCase(num)
+{
+	switch(num)
+	{
+		case 0: return 0;
+		break;
+		case 1: case 2: return 1;
+		break;
+		case 3: case 4: return 2;
+		break;
+		case 5: return 3;
+		break;
+		case 6: case 7: return 4;
+		break;
+		case 8: case 9: return 5;
+		break;
+		case 10: return 6;
+		break;
+		case 11: return 7;
+		break;
+		case 12: case 13: return 8;
+		break;
+		case 14: return 9;
+		break;
+		case 15: case 16: return 10;
+		break;
+		case 17: return 11;
+		break;
+		case 18: case 19: return 12;
+		break;
+		case 20: return 13;
+		break;
+		case 21: case 22: return 14;
+		break;
+		case 23: return 15;
+		break;
+		case 24: return 16;
+		break;
+	}
+}
+
 function loadImage(nom)
 {
 	var i,j;
 	var caseLetter;
 	var type;
 	fontImage=new Image();
-	fontImage="Images/"+nom+".BACK.PNG";
+	fontImage.src="Images/"+nom+".BACK.png";
 	for(i=0; i<tab.length; i++)
 	{
 		switch(i)
@@ -29,76 +70,104 @@ function loadImage(nom)
 					break;
 					case 1:caseLetter='B';
 					break;
-					case 2:caseLetter='C';
+					case 3:caseLetter='C';
 					break;
-					case 3:caseLetter='D';
+					case 5:caseLetter='D';
 					break;
-					case 4:caseLetter='E';
+					case 6:caseLetter='E';
 					break;
-					case 5:caseLetter='F';
+					case 8:caseLetter='F';
 					break;
-					case 6:caseLetter='G';
+					case 10:caseLetter='G';
 					break;
-					case 7:caseLetter='H';
+					case 11:caseLetter='H';
 					break;
-					case 8:caseLetter='I';
+					case 12:caseLetter='I';
 					break;
-					case 9:caseLetter='J';
+					case 14:caseLetter='J';
 					break;
-					case 10:caseLetter='K';
+					case 15:caseLetter='K';
 					break;
-					case 11:caseLetter='L';
+					case 17:caseLetter='L';
 					break;
-					case 12:caseLetter='M';
+					case 18:caseLetter='M';
 					break;
-					case 13:caseLetter='N';
+					case 20:caseLetter='N';
 					break;
-					case 14:caseLetter='O';
+					case 21:caseLetter='O';
 					break;
-					case 15:caseLetter='P';
+					case 23:caseLetter='P';
 					break;
-					case 16:caseLetter='Q';
+					case 24:caseLetter='Q';
 					break;
 					default: return null;
 
 				}
-		for(j=0; j<tab[i].length; j++)
+		// 1.1 Si c'est une images qui peut avoir une images de face ou qui uniquement une image de face (D,J,N)
+		if(caseLetter!='A' && caseLetter!='H' && caseLetter!='P' && caseLetter!='G' && caseLetter!='L' && caseLetter!='Q')
 		{
-			if(caseLetter!='D' && caseLetter!='J' && caseLetter!='N')
-			{
-				tab[i][j]=new Image();
-				tab[i][j].src="Images/"+nom+"."+caseLetter+"S"+j+".png";
-			}
-			else
+			for(j=0; j<tab[i].length; j++)
 			{
 				tab[i][j]=new Image();
 				tab[i][j].src="Images/"+nom+"."+caseLetter+"F"+j+".png";
 			}
-		}	
-
-		// CASE QUI PEUT ETRE DE FACE OU DE COTE 
+		}
+		//1.2 Si c'est une images hors du champ de vision
+		else
+		{
+			for(j=0; j<tab[i].length; j++)
+			{
+				tab[i][j]=new Image();
+				tab[i][j].src="Images/"+nom+"."+caseLetter+"S"+j+".png";
+			}
+		}
+		//1.3 CASE QUI PEUT ETRE DE FACE OU DE COTE (On leur rajoute l'image de cote car images de face deja ajouter au point 1.1)
 		if (caseLetter=='B' || caseLetter=='C' || caseLetter=='I' || caseLetter=='M' || caseLetter=='E' || caseLetter=='F' || 
 			caseLetter=='K' || caseLetter=='O') 
 		{
-			i++;
-			for(var k=i; k<tab.length; k++)
-			{
-				for(var l=0; l<tab[i].length; l++)
+			++i;
+			for(var l=0; l<tab[i].length; l++)
 				{
-					tab[k][l]=new Image();
-					tab[k][l].src="Images/"+nom+"."+caseLetter+"S"+l+".png";
+					tab[i][l]=new Image();
+					tab[i][l].src="Images/"+nom+"."+caseLetter+"S"+l+".png";
 				}	
-			}
 		}		
 	}
 }
 
-function drawCanva()
+function drawCanva(tableauCaseDevant)
 {
 	var canva=document.getElementById('canvagamescreen');
 	var ctx=canva.getContext('2d');
-	ctx.drawImage(tab[0][0],0,0,320,200);
-	ctx.drawImage(tab[1][0],20,0,320,200);
+	ctx.drawImage(fontImage,0,0,1280,800);
+	
+	// On affiche de A à C
+	for (var i = 0; i < 5; i++) {
+		ctx.drawImage(tab[i][tableauCaseDevant[indexOfCase(i)]],0,0,1155,665);
+	}
+	// On affiche de G à E et en dernier D
+	for (var i = 10; i >= 5; i--) {
+				ctx.drawImage(tab[i][tableauCaseDevant[indexOfCase(i)]],0,0,1155,665);
+	}
+	// On affiche de H à I
+	for (var i = 11; i < 14; i++) {
+				ctx.drawImage(tab[i][tableauCaseDevant[indexOfCase(i)]],0,0,1155,665);
+	}
+	// On affiche de L à K et en dernier J
+	for (var i = 17; i >= 14; i--) {
+				ctx.drawImage(tab[i][tableauCaseDevant[indexOfCase(i)]],0,0,1155,665);
+	}
+	// On affiche de M puis O puis N
+	for (var i = 18; i < 20; i++) {
+				ctx.drawImage(tab[i][tableauCaseDevant[indexOfCase(i)]],0,0,1155,665);
+	}
+	for (var i = 22; i >= 20; i--) {
+				ctx.drawImage(tab[i][tableauCaseDevant[indexOfCase(i)]],0,0,1155,665);
+	}
+	// On affiche P et Q
+	ctx.drawImage(tab[23][tableauCaseDevant[indexOfCase(23)]],0,0,1155,665);
+	ctx.drawImage(tab[24][tableauCaseDevant[indexOfCase(24)]],0,0,1155,665);
+	
 }
 
 
@@ -122,7 +191,9 @@ function switchToLogin()
 	var acceuil=document.getElementById("accueil");
 	acceuil.style.display='none';
 	login.style.display='block';
-	drawCanva();
+
+	var tabTest = [0,0,0,0,0,0,0,0,2,2,0,2,2,2,2,2,0];
+	drawCanva(tabTest);
 }
 
 
