ff6d157e4d082b48c471f0df5572d026735dd55c
Karim <karimblk07@gmail.com>
2021-02-15 14:26:32 +0100

    Ajout du fichier traitement.php

---
+++ traitement.php
@@ -0,0 +1,12 @@
+<?php
+// session_start();
+
+// 	//On met le joueur a la position initial (1,1)
+// 	$_SESSION['positionX']=1;
+// 	$_SESSION['positionY']=1;
+// 	//Recuperer les infos du formulaire de connexion
+// 	$pseudo = $_GET["pseudo"];
+// 	$y = $_GET["password"];
+	//On testera ici si le login est correct
+	echo "1";
+?>
